10a2cce2d833b75e14e942b5acdc308d744b53d4
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2021-03-27 20:41:38 +0900

    [update] 見た目を整える2

--- src/pages/[userId]/[articleId]/index.tsx
+++ src/pages/[userId]/[articleId]/index.tsx
@@ -1,7 +1,40 @@
 import { useRouter } from 'next/router'
 import { NextPage } from 'next'
+import Error from 'next/error'
+import styled from 'styled-components';
 
 import { useGetArticleQuery } from '@/generated/graphql'
+
+const Container = styled.div`
+  width: 700px;
+  margin: 0 auto;
+  font-size: 20px;
+  line-height: 30px;
+`;
+
+const Subject = styled.h1`
+  margin-top: 32px;
+  font-size: 40px;
+  line-height: 48px;
+`;
+
+const UserContainer = styled.div`
+  margin-top: 32px;
+  display: flex;
+`;
+
+const UserId = styled.div`
+  font-size: 16px;
+`;
+
+const UserName = styled.span`
+  font-size: 16px;
+  opacity: 0.5;
+`;
+
+const Content = styled.div`
+  margin-top: 32px;
+`;
 
 const ArticlePage: NextPage = () => {
     const router = useRouter()
@@ -19,7 +52,25 @@ const ArticlePage: NextPage = () => {
     if (error) {
         return <p>{error.toString()}</p>
     }
-    return <div>{JSON.stringify(data)}</div>
+
+    if (!data || !data.articles_by_pk) {
+        return <Error statusCode={404} />
+    }
+
+    const { user, subject, content } = data.articles_by_pk
+
+    return (
+        <Container>
+            <Subject>{subject}</Subject>
+            <UserContainer>
+                <div>
+                    <UserId>{user.displayId}</UserId>
+                    <UserName>{user.displayName}</UserName>
+                </div>
+            </UserContainer>
+            <Content>{content}</Content>
+        </Container>
+    )
 }
 
 export default ArticlePage
